4175a6cc7143ddc2174e8d9de9be5c509d31a683
mtezxx <118538867+mtezxx@users.noreply.github.com>
2023-03-03 14:47:18 +0100

    a

--- src/main/java/Model/BorrowedAndReturn.java
+++ src/main/java/Model/BorrowedAndReturn.java
@@ -1,5 +1,20 @@
 package Model;
 
-public class BorrowedAndReturn
+public class BorrowedAndReturn implements State
 {
+
+  @Override public void onBorrow(Vinyl vinyl)
+  {
+
+  }
+
+  @Override public void onReserved(Vinyl vinyl)
+  {
+
+  }
+
+  @Override public void onReturn(Vinyl vinyl)
+  {
+
+  }
 }
--- src/main/java/Model/Reserved.java
+++ src/main/java/Model/Reserved.java
@@ -6,21 +6,18 @@ public class Reserved implements State
 {
   @Override public void onBorrow(Vinyl vinyl)
   {
-    if (vinyl.getState()==Vinyl.State.RESERVED.getState()){
-      System.out.println("Vinyl is already reserved");
-    }
+
+
   }
 
   @Override public void onReserved(Vinyl vinyl)
   {
-    vinyl.setState(Vinyl.State.RESERVED);
+
   }
 
 
   @Override public void onReturn(Vinyl vinyl)
   {
-   if (vinyl.getState()==Vinyl.State.BORROWED.getState()){
-     vinyl.setState(Vinyl.State.AVAILABLE);
-   }
+
   }
 }
